c8054a09972a0bf8242783916035411a4048eecd
soundous <99813853+soundoussa3d@users.noreply.github.com>
2024-02-20 09:48:46 +0100

    day16

--- day16/cours.js
+++ day16/cours.js
@@ -1,3 +1,4 @@
+//npm install express
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -5,3 +6,13 @@ const port = 3000;
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`);
 });
+
+/*pagination :
+/products?limit=25&offset=50
+*/
+
+/*
+version
+/v1/products
+/v2/products
+*/ 
