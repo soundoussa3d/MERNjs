d6e6785e498d6214110db834f41960eca143cb81
soundous <99813853+soundoussa3d@users.noreply.github.com>
2024-02-22 00:18:35 +0100

    day 21

---
+++ day21/cours/cours.js
@@ -0,0 +1,27 @@
+//secure code version
+// Secured Node.js and Express server with input validation and sanitization
+const express = require('express');
+const app = express();
+const { body, validationResult } = require('express-validator');
+
+// Simulated database object
+let database = {};
+
+app.post('/submit-form', 
+  body('username').isLength({ min: 5 }).trim().escape(), 
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // User input is validated and sanitized
+    let username = req.body.username;
+    let email = req.body.email;
+    
+    // Simulating saving data to a 'database'
+    database[email] = { email };
+    database[username] = { username};
+    res.send(`User ${username} registered with email ${email}`);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
