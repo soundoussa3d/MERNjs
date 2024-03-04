da87dc21bb032f94d6cdb7848c61335f7a7b9e70
soundous <99813853+soundoussa3d@users.noreply.github.com>
2024-03-04 10:54:56 +0100

    quiz day 29 done

---
+++ day29/quiz/quiz.js
@@ -0,0 +1,194 @@
+//?1-What is MongoDB Atlas?
+//* A cloud-based platform for MongoDB
+
+//?In MongoDB Atlas, what does the term "Whitelist" refer to?
+//*A list of allowed IP addresses to connect to a clustery
+
+
+//?How can you implement advanced data validation in Mongoose using custom validation functions?
+//*By defining custom validation functions in the schema using the `validate` property
+
+
+
+
+/*
+//!i don't know the answer 
+How do you connect to a MongoDB Atlas Cluster from a Node.js application?
+ 
+ 
+Using the mongo connect command
+
+ 
+ 
+Configuring the connection string in the application
+
+ 
+ 
+Adding a new Atlas Cluster module
+
+ 
+ 
+Establishing a direct TCP connection
+
+
+////////////////////////////////////////////
+What is the purpose of the MongoDB Atlas M0?
+
+=>A testing environment with reduced resources
+
+///////////////////////////////////////////////////
+How can you handle errors in a Mongoose transaction?
+=>Using try-catch blocks
+
+ ///////////////////////////////////////////
+In MongoDB Atlas, a Cluster is:
+
+A group of MongoDB servers
+
+A cluster in MongoDB Atlas is a group of MongoDB servers that work together to store your data and provide high availability, fault tolerance, and scalability for your applications. It typically consists of multiple nodes (servers) that replicate data across each other to ensure data durability and availability.
+///////////////////////////////////////
+How can you enforce a unique constraint on a Mongoose schema field?
+ 
+Using the `unique` property
+
+ ///////////////////////////////////////
+
+What does the `populate()` method in Mongoose allow you to do?
+
+Retrieve referenced documents and populate them in a query result
+
+///////////////////////////////////////
+
+How do you deploy a Node.js application using MongoDB Atlas?
+
+By connecting the application to a MongoDB Atlas Cluster
+////////////////////////////////////////
+
+How do you enforce a minimum length validation on a string field in a Mongoose schema?
+ 
+ 
+Using the `minSize` property
+
+ 
+ 
+Using the `minlength` property*
+
+ 
+ 
+Using the `validate` property with a custom function
+
+ 
+ 
+Using the `minimum` property
+
+////////////////////////////////////
+
+What is the purpose of environment variables in the context of application deployment with MongoDB Atlas?
+ 
+ 
+To store sensitive information such as API keys***
+
+ 
+ 
+To define the structure of a MongoDB document
+
+ 
+ 
+To create a virtual environment for the application
+
+ 
+ 
+To configure the MongoDB Atlas dashboard
+
+
+
+////////////////////////////////////
+
+
+How can you use the `$regex` operator in Mongoose queries?
+ 
+ 
+To perform case-sensitive searches
+
+ 
+ 
+To perform regular expression-based searches*************
+
+ 
+ 
+To perform numeric comparisons
+
+ 
+ 
+To perform exact string matches
+
+
+
+////////////////////////////////////////////
+
+What does the `ref` property in a Mongoose schema indicate?
+ 
+The database reference for a field**************
+*/ 
+
+
+
+
+//!evaluation
+
+/*
+Which command is used to connect to a MongoDB server using the mongosh shell?
+ 
+ 
+connect("mongodb://localhost:27017")
+
+ 
+ 
+use("mydatabase")
+
+ 
+ 
+mongo connect localhost:27017
+
+ 
+ 
+mongosh***
+/////////////////////////////////
+How do you specify a condition for updating documents with the `update()` method in Mongoose?
+ 
+ 
+Using a query object
+
+ 
+ 
+By defining a schema
+
+ 
+ 
+By using a callback function
+
+ 
+ 
+By passing a string
+
+///////////////////////////////////////////
+
+What does an index in MongoDB consist of?
+ 
+ 
+Field data types
+
+ 
+ 
+Field names only
+
+ 
+ 
+Field values only
+
+ 
+ 
+Fields and their values
+
+
+*/ 
