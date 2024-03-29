971a2a2f4a7e2ff19b93ef02cbcf967b637e16db
soundous <99813853+soundoussa3d@users.noreply.github.com>
2024-03-29 14:10:05 +0000

    day48 cours part 2

--- day48/day48/src/App.jsx
+++ day48/day48/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './components/Counter'
+/*
+//Store : the global state 
+interface CounterState {
+  value:number
+}
+interface UserState{
+  isSignedIn:boolean
+}
+
+//Actions 
+const incrementByAmount ={
+  type:"INCREMENT",
+  payload:10
+};
+const decrement ={
+  type:"DECREMENT"
+};
+//Reducers :
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Counter/>
+    <h2>
+      Redux Complete Tutorial
+    </h2>
+      
     </>
   )
 }
